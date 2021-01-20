6b69187af3b646295b841f760460080f20a25b3a
d.lebid <d.lebid@program-ace.com.ua>
2021-01-20 22:42:50 +0200

    move job and job handle

--- Assets/Classic/Scripts/SpawnParallelManager.cs
+++ Assets/Classic/Scripts/SpawnParallelManager.cs
@@ -1,43 +1,75 @@
 ﻿using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Jobs;
 
 namespace Classic
 {
     public class SpawnParallelManager : MonoBehaviour
     {
+        // 1
+        struct MoveJob : IJobParallelForTransform
+        {
+            public void Execute(int index, TransformAccess transform)
+            {
+                transform.position += 10f * (transform.rotation * new Vector3(0, 0, 1));
+
+                if (transform.position.z > 50)
+                {
+                    transform.position =
+                        new Vector3(transform.position.x, 0, transform.position.z * -1);
+                }
+                else if (transform.position.z < -50)
+                {
+                    transform.position =
+                        new Vector3(transform.position.x, 0, transform.position.z * -1);
+                }
+
+            }
+        }
+
+        // 2
+        private MoveJob moveJob;
+        private JobHandle jobHandle;
+        private TransformAccessArray transforms;
+
         public GameObject Prefab;
         public float Speed = 0.1f;
         public int Count = 1000;
 
-        private GameObject[] prefabs;
+        private Transform[] prefabs;
 
-        void Start()
+        private void Start()
         {
-            prefabs = new GameObject[Count];
+            prefabs = new Transform[Count];
 
             for (int i = 0; i < Count; i++)
             {
                 Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
-                prefabs[i] = Instantiate(Prefab, pos, Quaternion.identity);
+                GameObject pref = Instantiate(Prefab, pos, Quaternion.identity);
+                prefabs[i] = pref.transform;
             }
+
+            // 3
+            transforms = new TransformAccessArray(prefabs);
         }
 
-        void Update()
+        private void Update()
         {
-            for (int i = 0; i < prefabs.Length; i++)
-            {
-                prefabs[i].transform.Translate(0, 0, Speed);
+            // 4
+            moveJob = new MoveJob();
+            jobHandle = moveJob.Schedule(transforms);
+        }
 
-                if (prefabs[i].transform.position.z > 50)
-                {
-                    prefabs[i].transform.position =
-                        new Vector3(prefabs[i].transform.position.x, 0, prefabs[i].transform.position.z * -1);
-                }
-                else if (transform.position.z < -50)
-                {
-                    prefabs[i].transform.position =
-                        new Vector3(prefabs[i].transform.position.x, 0, prefabs[i].transform.position.z * -1);
-                }
-            }
+        // 5
+        private void LateUpdate()
+        {
+            jobHandle.Complete();
+        }
+
+        // 6
+        private void OnDestroy()
+        {
+            transforms.Dispose();
         }
     }
 }
