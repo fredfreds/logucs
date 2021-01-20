046bfa523bc603944bc1be763b1093e61cbbe196
d.lebid <d.lebid@program-ace.com.ua>
2021-01-20 19:55:34 +0200

    add classic version

---
+++ Assets/Classic/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Classic
+{
+    public class Move : MonoBehaviour
+    {
+        public float Speed = 0.1f; 
+
+        void Update()
+        {
+            transform.Translate(0, 0, Speed);
+
+            if (transform.position.z > 50)
+            {
+                Speed *= -1;
+            }
+            else if (transform.position.z < -50)
+            {
+                Speed *= -1;
+            }
+        }
+    }
+}
---
+++ Assets/Classic/Scripts/SpawnManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Classic
+{
+    public class SpawnManager : MonoBehaviour
+    {
+        public GameObject Prefab;
+        public int Count = 1000;
+
+        void Start()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+                Instantiate(Prefab, pos, Quaternion.identity);
+            }
+        }
+    }
+}
